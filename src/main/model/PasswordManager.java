8a8871a3ac3528dbc38f75791502d69dded2f937
smeg24 <smeg24@students.cs.ubc.ca>
2025-02-05 18:45:47 -0800

    Added specification of PasswordManager class

---
+++ src/main/model/PasswordManager.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+// Manages a collection of passwords associated with 
+// different accounts 
+public class PasswordManager {
+
+
+
+
+}
