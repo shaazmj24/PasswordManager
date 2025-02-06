daebce76397b38f62cca87893bbd2e5652b172ef
smeg24 <smeg24@students.cs.ubc.ca>
2025-02-05 18:31:02 -0800

    Added specification for Password

--- src/main/model/Password.java
+++ src/main/model/Password.java
@@ -2,7 +2,20 @@ package model;
 
 // Represents a password associated with an account, 
 // with functionality to encrypt and decrypt it.
-public class Password {
+public class Password { 
+    private String password;  
+    private String account;         
+    private boolean isPassEncrypted; //whether password is encrypted (true) or not
+
+    // EFFECT: Constructs a Password object with the given password
+    // and account name and set isPassEncrypted to false .
+    public Password(String password, String account) { 
+
+    }
+
+    
+
+
     
 
 }
