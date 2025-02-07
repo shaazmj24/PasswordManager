7617bf5cb1501c6fd119df9784ab95c3219f1dd0
smeg24 <smeg24@students.cs.ubc.ca>
2025-02-06 23:29:29 -0800

    Added tests for PasswordManager

--- src/test/model/TestPasswordManager.java
+++ src/test/model/TestPasswordManager.java
@@ -41,7 +41,77 @@ public class TestPasswordManager {
 
     }
 
+    @Test 
+    void testAddPassAccMutiple() { 
+        passwords.addPasswordAccount(pass, pass.getAccount());
+        passwords.addPasswordAccount(pass1, pass1.getAccount()); 
+        assertEquals(pass1, passwords.searchPassword("ubc"));
+        assertEquals(pass, passwords.searchPassword("facebook"));
+        passwords.addPasswordAccount(pass2, pass2.getAccount());
+        assertTrue(pass2 == passwords.searchPassword("instagram"));
+    }
 
+    @Test 
+    void testSearchPassword() { 
+        passwords.addPasswordAccount(pass1, pass1.getAccount()); 
+        assertEquals(pass1, passwords.searchPassword("ubc"));
+        passwords.addPasswordAccount(pass2, pass2.getAccount());
+        assertEquals(pass2, passwords.searchPassword("instagram"));
+    }
+
+    @Test 
+    void testSearchPasswordIncorrect() {  
+        passwords.addPasswordAccount(pass1, pass1.getAccount()); 
+        passwords.addPasswordAccount(pass2, pass2.getAccount());
+        assertFalse(pass1 == passwords.searchPassword("instagram"));
+        assertFalse(pass2 == passwords.searchPassword("ubc"));
+    }
+
+    @Test 
+    void testDelete() {  
+        passwords.addPasswordAccount(pass, pass.getAccount());
+        assertEquals(pass, passwords.searchPassword("facebook"));
+        passwords.deletePassword("facebook");
+        assertTrue(passwords.listPasswords().size() == 0);
+        assertTrue(passwords.listAccounts().size() == 0);
+    }
+
+    @Test 
+    void testDeleteMutipleTimes() {  
+        passwords.addPasswordAccount(pass, pass.getAccount());
+        passwords.addPasswordAccount(pass1, pass1.getAccount()); 
+        passwords.addPasswordAccount(pass2, pass2.getAccount());
+        passwords.deletePassword("ubc"); 
+        assertTrue(passwords.listAccounts().size() == 2);
+        passwords.deletePassword("facebook");
+        assertTrue(passwords.listPasswords().size() == 1);
+        passwords.deletePassword("instagram");
+        assertTrue(passwords.listPasswords().size() == 0);
+    }
+
+    @Test 
+    void testListPasswords() { 
+        ArrayList<Password> listP = new ArrayList<>(); 
+        assertTrue(passwords.listPasswords().size() == 0);
+        passwords.addPasswordAccount(pass, pass.getAccount());
+        passwords.addPasswordAccount(pass1, pass1.getAccount());
+        assertTrue(passwords.listPasswords().size() == 2);
+        listP.add(pass); 
+        listP.add(pass1);
+        assertEquals(listP, passwords.listPasswords());
+    }
+
+    @Test  
+    void testListAccounts() { 
+        ArrayList<String> listA = new ArrayList<>(); 
+        assertTrue(passwords.listAccounts().size() == 0);
+        passwords.addPasswordAccount(pass, pass.getAccount());
+        passwords.addPasswordAccount(pass1, pass1.getAccount());
+        assertTrue(passwords.listAccounts().size() == 2);
+        listA.add("facebook"); 
+        listA.add("ubc");
+        assertEquals(listA, passwords.listAccounts());
+    }
     
 
 }
