1d6495b62c89c425157c1265eda50d44162c53f3
smeg24 <smeg24@students.cs.ubc.ca>
2025-02-13 23:28:56 -0800

    Added specification for PasswordApp

---
+++ src/main/ui/PasswordApp.java
@@ -0,0 +1,19 @@
+package ui;
+
+import model.Password;
+import model.PasswordManager;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class PasswordApp { 
+    private Password password;
+    private PasswordManager passwords;    
+
+    // Initializes PasswordManager and Runs the application
+    public PasswordApp() {  
+        
+    } 
+
+
+
+}
