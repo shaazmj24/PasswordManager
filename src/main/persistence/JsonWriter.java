45d0564a5674e5873b10abded1097f476db43cc1
smeg24 <smeg24@students.cs.ubc.ca>
2025-02-23 00:20:32 -0800

    Added specification of JsonWriter class

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,13 @@
+package persistence;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.util.List;
+
+// Represents a writer that writes JSON representation of 
+// passwormanager to file
+public class JsonWriter {
+    
+}
