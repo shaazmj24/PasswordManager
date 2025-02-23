dae3577926d9bfad79784ebdd28fbf1ee4c54183
smeg24 <smeg24@students.cs.ubc.ca>
2025-02-23 11:02:43 -0800

    Added specifiction for JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -9,5 +9,17 @@ import java.util.List;
 // Represents a writer that writes JSON representation of 
 // passwormanager to file
 public class JsonWriter {
+    private PrintWriter writer; 
+    private String destination; 
+    private static final int TAB = 4;
+
+    // EFFECT: constructs writer to write to destination file 
+    public JsonWriter(String destination) { 
+
+    }
+
+
+
+
     
 }
