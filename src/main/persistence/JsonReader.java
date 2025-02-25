ca7a4c4ac692ddfedc01c6af58b386146e5b3482
smeg24 <smeg24@students.cs.ubc.ca>
2025-02-25 02:52:43 -0800

    added specification of jsonReader class

---
+++ src/main/persistence/JsonReader.java
@@ -0,0 +1,14 @@
+package persistence;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// A reader that can read passwords from a file 
+public class JsonReader {  
+
+}
