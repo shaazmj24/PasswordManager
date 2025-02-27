efab0142129058498fd2dbe8d95dd444d9ce9262
smeg24 <smeg24@students.cs.ubc.ca>
2025-02-27 02:16:13 -0800

    added specification of readPassword

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -2,6 +2,9 @@ package persistence;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import model.PasswordManager;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -10,7 +13,12 @@ import java.util.Scanner;
 
 // A reader that can read passwords from a file 
 public class JsonReader {  
-    private static final String DELIMITER = ",";   
-    
+    private static final String DELIMITER = ",";    
+
+    // EFFECT: returns a list of passwords from file; throws 
+    // IOException if an exception is caught when opening or reading file 
+    public static PasswordManager readPasswords(File file) throws IOException {  
+
+    }
 
 }
