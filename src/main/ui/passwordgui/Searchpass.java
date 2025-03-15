21a49e0a71b0ae8c596c81e03b0e68b8df57e75d
smeg24 <smeg24@students.cs.ubc.ca>
2025-03-15 15:42:04 -0700

    Created searchpass class

---
+++ src/main/ui/passwordgui/Searchpass.java
@@ -0,0 +1,14 @@
+package ui.passwordgui;
+import javax.swing.*;
+
+public class Searchpass { 
+    private JButton button3; 
+
+    public Searchpass() {  
+        button3 = new JButton("Search Password"); 
+    }
+
+    public JButton getButton() { 
+        return button3; 
+    }
+}
