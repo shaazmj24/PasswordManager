7607a656db48d016e29b1a4a0d1bd445a370bcc3
smeg24 <smeg24@students.cs.ubc.ca>
2025-03-15 15:41:08 -0700

    Created Deletepass class

---
+++ src/main/ui/passwordgui/Deletepass.java
@@ -0,0 +1,15 @@
+package ui.passwordgui;
+
+import javax.swing.JButton;
+
+public class Deletepass {  
+    private JButton button4; 
+
+    public Deletepass() {  
+        button4 = new JButton("Delete Password"); 
+    }
+
+    public JButton getButton() { 
+        return button4; 
+    }
+}
