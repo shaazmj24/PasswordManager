919691f4682ffc9bca22fc05093929a6a0f53241
smeg24 <smeg24@students.cs.ubc.ca>
2025-03-15 15:41:31 -0700

    Created Encryptpass class

---
+++ src/main/ui/passwordgui/Encryptpass.java
@@ -0,0 +1,14 @@
+package ui.passwordgui;
+import javax.swing.*;
+
+public class Encryptpass { 
+    private JButton button2; 
+
+    public Encryptpass() {  
+        button2 = new JButton("Encrypt Password"); 
+    }
+
+    public JButton getButton() { 
+        return button2; 
+    }
+}
