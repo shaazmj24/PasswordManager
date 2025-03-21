df6f9041a68cbe7ec5df0ef0b2a793d45ae71876
smeg24 <smeg24@students.cs.ubc.ca>
2025-03-20 19:37:23 -0700

    Added MainPanel

--- src/main/ui/passwordgui/Launcher.java
+++ src/main/ui/passwordgui/Launcher.java
@@ -5,25 +5,34 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Launcher { 
+    private static JPanel mainPanel; 
     public static void main(String[] args) {  
         // creates Frame 
         JFrame frame = new JFrame();  
         frame.setTitle("Password Manager"); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application  
-        frame.setSize(400, 400); 
-        frame.setVisible(true);     
+        frame.setSize(400, 400);  
+        
+        // JPanel = Gui component that functions as a container to hold other component
+        // create panel with cardlayout which switch pages within this frame
+        mainPanel = new JPanel(new CardLayout()); 
 
-        // JPanel = Gui component that functions as a container to hold other components
-        JPanel panel = new JPanel();  
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); // Y_axis for vertical alignment
-        addButtons(panel, 30); 
-        // add panel to frame 
-        frame.add(panel);
+        // create menu panel 
+        JPanel mainMenuPanel = new JPanel(); 
+        mainMenuPanel.setLayout(new BoxLayout(mainMenuPanel, BoxLayout.Y_AXIS)); // Y_axis for vertical alignment
+
+        addButtons(mainPanel, mainMenuPanel, 30); 
+        // add panels to the main panel with cardlayout 
+        mainPanel.add(mainMenuPanel, "MainMenu"); 
+
+        // add mainpanel to frame 
+        frame.add(mainPanel);
+        frame.setVisible(true);
     }
  
     // EFFECT: add buttons to panel
-    public static void addButtons(JPanel panel, int space) { 
-        Storepass button1 = new Storepass(); 
+    public static void addButtons(JPanel mainPanel, JPanel panel, int space) { 
+        Storepass button1 = new Storepass(mainPanel); 
         Encryptpass button2 = new Encryptpass();
         Searchpass button3 = new Searchpass();
         Deletepass button4 = new Deletepass();
@@ -51,4 +60,8 @@ public class Launcher {
 
  
 }
- 
+
+
+
+
+
