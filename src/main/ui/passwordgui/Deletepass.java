b0f3749e5ee44522c5efffbe5860b917ddbdec10
smeg24 <smeg24@students.cs.ubc.ca>
2025-03-22 16:15:02 -0700

    implemented delete gui

--- src/main/ui/passwordgui/Deletepass.java
+++ src/main/ui/passwordgui/Deletepass.java
@@ -1,15 +1,127 @@
 package ui.passwordgui;
 
-import javax.swing.JButton;
+import model.Password;
+import model.PasswordManager;
 
-public class Deletepass {  
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import ui.passwordgui.Storepass;
+
+public class Deletepass extends JPanel implements ActionListener {  
     private JButton button4; 
+    private JButton delete; 
+    private JPanel mainPanel; 
+    private JTextField enterAccount; 
+    private JButton back; 
+    private PasswordManager pm;
 
-    public Deletepass() {  
+    public Deletepass(JPanel mainPanel, PasswordManager pm) {  
         button4 = new JButton("Delete Password"); 
+        button4.addActionListener(this); 
+        delete = new JButton("Delete"); 
+        delete.addActionListener(this);  
+        this.mainPanel = mainPanel; 
+        this.pm = pm; 
+        back = new JButton("Back"); 
+        back.addActionListener(this); 
+    }  
+
+    @Override 
+    public void actionPerformed(ActionEvent e) {  
+        if (e.getSource() == button4) {  
+            openNewPage(); // call method to create new frame 
+        } 
+        if (e.getSource() == delete) {   
+            String account = enterAccount.getText().trim();  
+            deletePassword(account);
+        }
+        if (e.getSource() == back) {  
+            returnMain(); 
+        }
     }
+
+    public void openNewPage() {  
+        JPanel newPage = new JPanel();  
+        newPage.setLayout(new GridBagLayout()); 
+        newPage.setVisible(true);
+        content(newPage); 
+        mainPanel.add(newPage, "delete"); 
+
+        CardLayout cardLayout = (CardLayout) mainPanel.getLayout();  
+        cardLayout.show(mainPanel, "delete");
+    }
+
+    public void content(JPanel newPage) {  
+        GridBagConstraints gbc = new GridBagConstraints(); 
+        // Label1: Enter Account
+        JLabel label1 = new JLabel("Enter Account:");  
+        label1.setFont(new Font("Arial", Font.BOLD, 23)); 
+        gbc.gridx = 0; 
+        gbc.gridy = 1; 
+        gbc.anchor = GridBagConstraints.LAST_LINE_END; 
+        gbc.insets = new Insets(10, 10, 10, 10); 
+        newPage.add(label1, gbc);
+
+        // Account field  
+        enterAccount = new JTextField(); 
+        enterAccount.setPreferredSize(new Dimension(250, 40));
+        gbc.gridx = 1; 
+        gbc.gridy = 1; 
+        gbc.anchor = GridBagConstraints.LAST_LINE_START; 
+        newPage.add(enterAccount, gbc); 
+
+        // sumbit button  
+        gbc.gridx = 1; 
+        gbc.gridy = 2; 
+        gbc.anchor = GridBagConstraints.CENTER; 
+        newPage.add(delete, gbc); 
+    }
+
+    public void deletePassword(String acc) {       
+        String message = pm.deletePassword(acc); 
+        JLabel deleteLabel = new JLabel(message); 
+        deleteLabel.setFont(new Font("Arial", Font.BOLD, 23)); 
+        JPanel deletePanel = new JPanel(new GridBagLayout()); 
+        deletePanel.setVisible(true); 
+
+        GridBagConstraints gbc = new GridBagConstraints(); 
+        // add label 
+        gbc.gridx = 0; 
+        gbc.gridy = 0; 
+        gbc.insets = new Insets(10, 10, 10, 10); 
+        gbc.anchor = GridBagConstraints.CENTER; 
+        deletePanel.add(deleteLabel, gbc); 
+
+        // add back below 
+        gbc.gridy = 1; 
+        deletePanel.add(back, gbc); 
+
+        mainPanel.add(deletePanel, "d");
+        CardLayout c = (CardLayout) mainPanel.getLayout(); 
+        c.show(mainPanel, "d"); 
+    }
+
+    public void returnMain() {  
+        CardLayout c = (CardLayout) mainPanel.getLayout(); 
+        c.show(mainPanel, "MainMenu"); 
+    }
+
 
     public JButton getButton() { 
         return button4; 
     }
+
+
+
+
 }
+
+
+
+
+
